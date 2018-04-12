6a7bf4e78ada9c760063edffd41051ca2e2a116b
Craig Phares <craigphares@sixoverground.com>
2018-04-12 16:04:04 -0400

    initial import

---
+++ app/omniauth/stategies/knockknock.rb
@@ -0,0 +1,38 @@
+require 'omniauth-oauth2'
+
+puts 'loaded omniauth strategy'
+
+module OmniAuth
+  module Strategies
+    class Knockknock < OmniAuth::Strategies::OAuth2
+      # change the class name and the :name option to match your application name
+      option :name, :knockknock
+
+      option :client_options, {
+        :site => "http://localhost:3000",
+        :authorize_url => "/oauth/authorize"
+      }
+
+      uid { raw_info["id"] }
+
+      info do
+        {
+          :email => raw_info["email"]
+          # and anything else you want to return to your API consumers
+        }
+      end
+
+      def raw_info
+        puts "get raw_info"
+        @raw_info ||= access_token.get('/api/v1/me.json').parsed
+      end
+
+      # https://github.com/intridea/omniauth-oauth2/issues/81
+      def callback_url
+        url = full_host + script_name + callback_path
+        puts "callback_url=#{url}"
+        return url
+      end
+    end
+  end
+end
